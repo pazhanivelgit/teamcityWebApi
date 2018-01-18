96db20c6f490e0dbc953ab7b3b098f463bb257c6
Vel <pazhanivel.chidambaram@groupimd.com>
2018-01-18 11:23:04 +0800

    change in master-1

--- webApi/master.cs
+++ webApi/master.cs
@@ -9,7 +9,7 @@ namespace webApi
 
 //tested changed in master - conflict solved    public class Class1
     {
-		//add in master
+		//add in master - again added into master
     }
 	//add in branch
 }
